57e53d0c8ced2e60277abbb9009bdc64c2ec54b0
abdulkhamid <abdulkhamid1828@gmail.com>
2022-04-21 10:46:01 +0500

    fix

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -8,24 +8,12 @@ import Testimonial from "../../components/testimonial/Testimonial";
 import Contact from "../../components/contact/Contact";
 import Callsection from "../../components/callSection/Callsection";
 import { StyledMain } from "./Main.style";
-import { StyledConatiner } from "../../components/container/Container.style";
 import Footer from "../../components/footer/Footer";
 
 function Main() {
   return (
     <StyledMain>
       <Header />
-      <div class="container style_container">
-        <div class="line_frame2">
-          <div>
-            <div className="line_1"></div> 
-            <div className="line_2"></div>
-            <div className="line_3"></div>
-            <div className="line_4"></div> 
-            <div className="line_5"></div>
-          </div>
-        </div>
-      </div>
       <TopSection />
       <Portfolio />
       <Services />
--- src/pages/main/Main.style.js
+++ src/pages/main/Main.style.js
@@ -20,7 +20,7 @@ export const StyledMain = styled.section`
   }
     .line_frame2 {
       width: 100%;
-      height: 1000%;
+      height: 100%;
       overflow: hidden;
       div{
         display: -webkit-box;
@@ -35,126 +35,6 @@ export const StyledMain = styled.section`
         justify-content: space-between;
         padding: 0 15px;
         height: 100%;
-        .line_1{
-          &:before{
-            -webkit-animation-name: lineAnimation;
-            -moz-animation-name: lineAnimation;
-            -o-animation-name: lineAnimation;
-            animation-name: lineAnimation;
-            -webkit-animation-duration: 40s;
-            -moz-animation-duration: 40s;
-            -o-animation-duration: 40s;
-            animation-duration: 40s;
-            -webkit-animation-iteration-count: infinite;
-            -moz-animation-iteration-count: infinite;
-            -o-animation-iteration-count: infinite;
-            animation-iteration-count: infinite;
-            -webkit-animation-timing-function: linear;
-            -moz-animation-timing-function: linear;
-            -o-animation-timing-function: linear;
-            animation-timing-function: linear;
-            -webkit-animation-delay: 3s;
-            -moz-animation-delay: 3s;
-            -o-animation-delay: 3s;
-            animation-delay: 3s;
-          }
-        }
-        .line_2{
-          &:before{
-            -webkit-animation-name: lineAnimation;
-            -moz-animation-name: lineAnimation;
-            -o-animation-name: lineAnimation;
-            animation-name: lineAnimation;
-            -webkit-animation-duration: 40s;
-            -moz-animation-duration: 40s;
-            -o-animation-duration: 40s;
-            animation-duration: 40s;
-            -webkit-animation-iteration-count: infinite;
-            -moz-animation-iteration-count: infinite;
-            -o-animation-iteration-count: infinite;
-            animation-iteration-count: infinite;
-            -webkit-animation-timing-function: linear;
-            -moz-animation-timing-function: linear;
-            -o-animation-timing-function: linear;
-            animation-timing-function: linear;
-            -webkit-animation-delay: 7s;
-            -moz-animation-delay: 7s;
-            -o-animation-delay: 7s;
-            animation-delay: 7s;
-          }
-        }
-        .line_3{
-          &:before{
-            -webkit-animation-name: lineAnimation;
-            -moz-animation-name: lineAnimation;
-            -o-animation-name: lineAnimation;
-            animation-name: lineAnimation;
-            -webkit-animation-duration: 40s;
-            -moz-animation-duration: 40s;
-            -o-animation-duration: 40s;
-            animation-duration: 40s;
-            -webkit-animation-iteration-count: infinite;
-            -moz-animation-iteration-count: infinite;
-            -o-animation-iteration-count: infinite;
-            animation-iteration-count: infinite;
-            -webkit-animation-timing-function: linear;
-            -moz-animation-timing-function: linear;
-            -o-animation-timing-function: linear;
-            animation-timing-function: linear;
-            -webkit-animation-delay: 11s;
-            -moz-animation-delay: 11s;
-            -o-animation-delay: 11s;
-            animation-delay: 11s;
-          }
-        }
-        .line_4{
-          &:before{
-            -webkit-animation-name: lineAnimation;
-            -moz-animation-name: lineAnimation;
-            -o-animation-name: lineAnimation;
-            animation-name: lineAnimation;
-            -webkit-animation-duration: 40s;
-            -moz-animation-duration: 40s;
-            -o-animation-duration: 40s;
-            animation-duration: 40s;
-            -webkit-animation-iteration-count: infinite;
-            -moz-animation-iteration-count: infinite;
-            -o-animation-iteration-count: infinite;
-            animation-iteration-count: infinite;
-            -webkit-animation-timing-function: linear;
-            -moz-animation-timing-function: linear;
-            -o-animation-timing-function: linear;
-            animation-timing-function: linear;
-            -webkit-animation-delay: 8s;
-            -moz-animation-delay: 8s;
-            -o-animation-delay: 8s;
-            animation-delay: 8s;
-          }
-        }
-        .line_5{
-          &:before{
-            -webkit-animation-name: lineAnimation;
-            -moz-animation-name: lineAnimation;
-            -o-animation-name: lineAnimation;
-            animation-name: lineAnimation;
-            -webkit-animation-duration: 40s;
-            -moz-animation-duration: 40s;
-            -o-animation-duration: 40s;
-            animation-duration: 40s;
-            -webkit-animation-iteration-count: infinite;
-            -moz-animation-iteration-count: infinite;
-            -o-animation-iteration-count: infinite;
-            animation-iteration-count: infinite;
-            -webkit-animation-timing-function: linear;
-            -moz-animation-timing-function: linear;
-            -o-animation-timing-function: linear;
-            animation-timing-function: linear;
-            -webkit-animation-delay: 4s;
-            -moz-animation-delay: 4s;
-            -o-animation-delay: 4s;
-            animation-delay: 4s;
-          }
-        }
         div{
           border-left: 1px solid hsla(0,0%,64%,.2);
           width: 0.1px;
@@ -179,5 +59,4 @@ export const StyledMain = styled.section`
       }
     }
 
-  @keyframes lineAnimation{0%{top:0}to{top:100%}}
 `;
